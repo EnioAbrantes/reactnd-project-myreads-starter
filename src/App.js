3dc7396c7b5b3411b220ac1455c6b970760dea95
esilva16 <esilva16@lenovo.com>
2019-01-30 15:20:12 -0300

    Removing the state of showPages and adding the react router to the project.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Link } from 'react-router-dom'
-//import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Book from './Components/Book'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+  state = {}
 
+  handleSearch = () => {
+    this.setState({ showSearchPage: !this.state.showSearchPage })
+  } 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route exact path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <Link to='/'> 
+                <button className="close-search">Close</button> 
+              </Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -40,7 +37,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -91,15 +89,13 @@ class BooksApp extends React.Component {
                 </div>
               </div>
             </div>
-            
-            {/* <Route exact path='/search' render={() => {this.setState({ showSearchPage: true })}} />*/}
 
             <Link to='/search' className="open-search">
               <button>Add a book</button>
             </Link>
 
           </div>
-        )}
+        )} />
       </div>
     )
   }

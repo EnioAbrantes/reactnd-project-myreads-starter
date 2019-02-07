4bd86cdda7e6018786044c0eaf8da1125abc440d
esilva16 <esilva16@lenovo.com>
2019-02-07 15:58:19 -0300

    Showing the books according the search

--- src/App.js
+++ src/App.js
@@ -15,18 +15,13 @@ class BooksApp extends React.Component {
     read : [<Book updateShelf={this.updateShelf} bookWriter= {'J.R.R. Tolkien'} bookName={'The Hobbit'}style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}/>,
     <Book updateShelf={this.updateShelf} bookWriter= {'Seuss'} bookName={'Oh, the Places You\'ll Go!'}style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}/>,
     <Book updateShelf={this.updateShelf} bookWriter= {'Mark Twain'} bookName={'The Adventures of Tom Sawyer'}style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}/>],
-    query : [],
+    query : '',
     queryResult : []
   }
   
   componentDidMount(){
-    console.log('bar')
-     /* BooksAPI.search('android').then((books) => (
-      books.map((book) => (
-        //console.log(book)
-        this.setState((state) => { return {queryResult : state.queryResult.concat([<Book bookWriter={book.authors} bookName={book.title} style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>])}}) 
-      ))
-    ))  */
+    console.log('bar');
+    //this.showingBooks();
 
   }
 
@@ -44,17 +39,25 @@ class BooksApp extends React.Component {
   }
 
   updateQuery = function(query) {
+    
+    this.setState(({query : query, queryResult : []}))
     console.log(this.state.query)
-    this.setState({query : query})
+    this.showingBooks();
   }
 
-  showingBooks = function(){ 
-    BooksAPI.search(this.state.query).then((books) => (
-    books.map((book) => (
-      //console.log(book)
-      this.setState((state) => { return {queryResult : state.queryResult.concat([<Book bookWriter={book.authors} bookName={book.title} style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>])}}) 
-    ))
-  )) }
+  showingBooks = function(){
+    console.log('bla')
+
+    BooksAPI.search(this.state.query).then((books) => {
+      console.log('books' + books)
+      books && books.map((book) => {
+        this.setState((state) => { 
+          console.log('state' +state)
+          return {queryResult : state.queryResult.concat([<Book bookWriter={book.authors} bookName={book.title} style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>])}
+        }) 
+      })
+    })
+  }
 
   render() {
     
@@ -94,9 +97,13 @@ class BooksApp extends React.Component {
             </div>
             <div className="search-books-results">
               <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {console.log(this.showingBooks())}
-                  </ol>
+                <ol className="books-grid">
+                  {this.state.queryResult.filter((book) => (
+                    <li key={book.props.bookName}>
+                      {book}
+                    </li>
+                  )) }
+                </ol>
               </div>
             </div>
           </div>

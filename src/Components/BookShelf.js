a6dfb6f15d2a39de874cc625a377ef3cef0d4123
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-02-18 22:05:57 -0300

    Making the component BookShelf a functional stateless component.

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
-class BookShelf extends React.Component {
-    
-    updateSelect = function(event,shelf){
-        shelf ? event.target.value = shelf : event.target.value = 'none'
-    }
-    
-    render(){
-        return(
-        <div className="book-shelf-changer" >
-            <select onFocus={(e) => this.updateSelect(e,this.props.bookShelf)} onChange={(e) => this.props.updateShelf(e)}>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading" >Currently Reading</option>
-                <option value="wantToRead" >Want to Read</option>
-                <option value="read" >Read</option>
-                <option value="none">None</option>
-            </select>
-        </div>)
-    }
+const BookShelf = (props) => {
+    return(
+    <div className="book-shelf-changer" >
+        <select onFocus={(e) => props.bookShelf ? e.target.value = props.bookShelf : e.target.value = 'none'} onChange={(e) => props.updateShelf(e)}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading" >Currently Reading</option>
+            <option value="wantToRead" >Want to Read</option>
+            <option value="read" >Read</option>
+            <option value="none">None</option>
+        </select>
+    </div>)
 }
-
 
 export default BookShelf;
